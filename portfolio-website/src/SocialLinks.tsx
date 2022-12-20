c1e3811ac88950805b6329be232ad3eb02c10ff5
Ellie <101657425+elliehl@users.noreply.github.com>
2022-12-20 17:44:51 +0000

    Create SocialLinks component

---
+++ portfolio-website/src/SocialLinks.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function SocialLinks() {
+    return <div className="SocialLinks">
+      <h3>GitHub</h3>
+      <h3>LinkedIn</h3>
+    </div>;
+  }
+
+  export default SocialLinks
