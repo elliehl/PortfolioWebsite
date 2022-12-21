3c0d3809c16607778f5decc094cfd01ccecaa507
Ellie <101657425+elliehl@users.noreply.github.com>
2022-12-21 00:00:14 +0000

    Restructure files

--- portfolio-website/src/App.tsx
+++ portfolio-website/src/App.tsx
@@ -1,9 +1,10 @@
 import './Styles/App.css'
 import React from 'react'
+import SocialLinks from './SocialLinks';
 
 function App() {
   return <div className="App">
-    <h1>Portfolio</h1>
+    <SocialLinks />
   </div>;
 }
 
