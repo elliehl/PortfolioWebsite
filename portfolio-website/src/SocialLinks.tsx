5ed8ddd91e426047a5cedde6e84aba6997541665
Ellie <101657425+elliehl@users.noreply.github.com>
2022-12-21 00:00:38 +0000

    Add links

--- portfolio-website/src/SocialLinks.tsx
+++ portfolio-website/src/SocialLinks.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './Styles/SocialLinks.css'
 
 function SocialLinks() {
     return <div className="SocialLinks">
-      <h3>GitHub</h3>
-      <h3>LinkedIn</h3>
+      <a href="https://github.com/elliehl" target="_blank" className="ExternalLink">GitHub</a>
+      <a href="https://uk.linkedin.com/in/ellie-lyons-644302258" target="_blank" className="ExternalLink">LinkedIn</a>
     </div>;
   }
 
