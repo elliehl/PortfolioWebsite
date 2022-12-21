bb5550826f7e4987c71f9f8b4b20ddad7e9e9029
Ellie <101657425+elliehl@users.noreply.github.com>
2022-12-21 20:18:15 +0000

    Add NavBar back

---
+++ portfolio-website/components/NavBar.tsx
@@ -0,0 +1,10 @@
+function NavBar() {
+    return (
+    <div className="AllSocialLinks">
+    <a href="https://github.com/elliehl" target='_blank' className="SocialLink">GitHub</a>
+    <a href="https://uk.linkedin.com/in/ellie-lyons-644302258" target='_blank' className="SocialLink">LinkedIn</a>
+    </div>
+    )
+}
+
+export default NavBar;
--- portfolio-website/pages/_app.tsx
+++ portfolio-website/pages/_app.tsx
@@ -1,8 +1,11 @@
+import '../styles/App.css'
+import '../styles/NavBar.module.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import NavBar from '../components/NavBar'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
-  Test
+  <NavBar />
   <Component {...pageProps} /></>
 }
